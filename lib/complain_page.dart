45df79e950f33eab881c08aebc588dbba7a29880
silvaman47 <57997678+silvaman47@users.noreply.github.com>
2022-07-17 09:38:36 +0000

    added image picker

--- lib/complain_page.dart
+++ lib/complain_page.dart
@@ -1,4 +1,9 @@
+// ignore_for_file: prefer_const_constructors, deprecated_member_use, avoid_unnecessary_containers, prefer_const_literals_to_create_immutables
+
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class ComplainPage extends StatefulWidget {
   const ComplainPage({Key? key}) : super(key: key);
@@ -8,20 +13,57 @@ class ComplainPage extends StatefulWidget {
 }
 
 class _ComplainPageState extends State<ComplainPage> {
+  File? _image;
+
+  final _picker = ImagePicker();
+  // Implementing the image picker
+  Future<void> _openImagePicker() async {
+    final XFile? pickedImage =
+        await _picker.pickImage(source: ImageSource.gallery);
+    if (pickedImage != null) {
+      setState(() {
+        _image = File(pickedImage.path);
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Complain'),
+        title: const Text('Complain'),
       ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            SizedBox(
-              height: 20,
-            ),
-            Text('Add Image'),
-          ],
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(35),
+          child: Column(
+            children: <Widget>[
+              Center(
+                child: ElevatedButton(
+                  child: const Text('Select An Image'),
+                  onPressed: _openImagePicker,
+                ),
+              ),
+              const SizedBox(height: 35),
+              Container(
+                alignment: Alignment.center,
+                width: double.infinity,
+                height: 300,
+                color: Colors.grey[300],
+                child: _image != null
+                    ? Image.file(_image!, fit: BoxFit.cover)
+                    : const Text('Please select an image'),
+              ),
+              SizedBox(
+                height: 40,
+              ),
+              Row(
+                children: [Text('Location'),
+                
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
