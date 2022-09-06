8de0dda45f925884f39cb80d35cd9f814d877c72
silvaman47 <57997678+silvaman47@users.noreply.github.com>
2022-09-06 12:03:35 +0000

    stuff

--- lib/screens/login/loginpage.dart
+++ lib/screens/login/loginpage.dart
@@ -21,128 +21,129 @@ class _LoginpageState extends State<Loginpage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: SingleChildScrollView(
-      child: Column(
-          // ignore: prefer_const_literals_to_create_immutables
-          children: <Widget>[
-            Column(children: <Widget>[
-              Row(
-                children: [
-                  Container(
-                    margin: EdgeInsets.all(8),
-                    child: IconButton(
-                        onPressed: null, icon: Icon(Icons.arrow_back_ios)),
-                  ),
-                ],
-              ),
-              SizedBox(
-                height: 30,
-              ),
-              Row(
-                children: [
-                  Container(
-                    margin: EdgeInsets.only(top: 10, left: 50),
-                    child: Text(
-                      'Login',
-                      style:
-                          TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+      body: SingleChildScrollView(
+        child: Column(
+            // ignore: prefer_const_literals_to_create_immutables
+            children: <Widget>[
+              Column(children: <Widget>[
+                Row(
+                  children: [
+                    Container(
+                      margin: EdgeInsets.all(8),
+                      child: IconButton(
+                          onPressed: null, icon: Icon(Icons.arrow_back_ios)),
                     ),
-                  ),
-                ],
-              ),
-              SizedBox(
-                height: 30,
-              ),
-              Row(
-                children: <Widget>[
-                  Container(
-                    margin: EdgeInsets.only(left: 40),
-                    child: Text(
-                      'Your Email',
-                      style: TextStyle(fontWeight: FontWeight.bold),
-                    ),
-                  ),
-                ],
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              Container(
-                height: MediaQuery.of(context).size.height * 0.10,
-                width: MediaQuery.of(context).size.width * 0.80,
-                child: TextField(
-                  controller: emailController,
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: 'Email',
-                  ),
+                  ],
                 ),
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              Row(
-                children: <Widget>[
-                  Container(
-                    margin: EdgeInsets.only(left: 40),
-                    child: Text(
-                      'Password',
-                      style: TextStyle(fontWeight: FontWeight.bold),
-                    ),
-                  ),
-                ],
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              Container(
-                height: MediaQuery.of(context).size.height * 0.10,
-                width: MediaQuery.of(context).size.width * 0.80,
-                child: TextField(
-                  controller: passwordController,
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: 'Password',
-                  ),
+                SizedBox(
+                  height: 30,
                 ),
-              ),
-              SizedBox(
-                height: 40,
-              ),
-              Container(
-                height: MediaQuery.of(context).size.height * 0.10,
-                width: MediaQuery.of(context).size.width * 0.80,
-                child: Center(
-                    child: InkWell(
-                  child: Text('Login'),
-                  onTap: () {
-                    try {
-                      auth.signInWithEmailAndPassword(
-                          email: emailController.text.trim(),
-                          password: passwordController.text);
-                    } on FirebaseAuthException catch (e) {
-                      log(e.toString());
-                      CustomDialog(
-                          onPressed: () {
-                            Navigator.pop(context);
-                          },
-                          content: e.toString());
-                    }
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => const Homepage(),
+                Row(
+                  children: [
+                    Container(
+                      margin: EdgeInsets.only(top: 10, left: 50),
+                      child: Text(
+                        'Login',
+                        style: TextStyle(
+                            fontSize: 30, fontWeight: FontWeight.bold),
                       ),
-                    );
-                  },
-                )),
-                decoration: BoxDecoration(
-                  color: Colors.blue,
-                  borderRadius: BorderRadius.circular(8),
+                    ),
+                  ],
                 ),
-              ),
+                SizedBox(
+                  height: 30,
+                ),
+                Row(
+                  children: <Widget>[
+                    Container(
+                      margin: EdgeInsets.only(left: 40),
+                      child: Text(
+                        'Your Email',
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Container(
+                  height: MediaQuery.of(context).size.height * 0.10,
+                  width: MediaQuery.of(context).size.width * 0.80,
+                  child: TextField(
+                    controller: emailController,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Email',
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Row(
+                  children: <Widget>[
+                    Container(
+                      margin: EdgeInsets.only(left: 40),
+                      child: Text(
+                        'Password',
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Container(
+                  height: MediaQuery.of(context).size.height * 0.10,
+                  width: MediaQuery.of(context).size.width * 0.80,
+                  child: TextField(
+                    controller: passwordController,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Password',
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 40,
+                ),
+                Container(
+                  height: MediaQuery.of(context).size.height * 0.10,
+                  width: MediaQuery.of(context).size.width * 0.80,
+                  child: Center(
+                      child: InkWell(
+                    child: Text('Login'),
+                    onTap: () {
+                      try {
+                        auth.signInWithEmailAndPassword(
+                            email: emailController.text.trim(),
+                            password: passwordController.text);
+                      } on FirebaseAuthException catch (e) {
+                        log(e.toString());
+                        CustomDialog(
+                            onPressed: () {
+                              Navigator.pop(context);
+                            },
+                            content: e.toString());
+                      }
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => const Homepage(),
+                        ),
+                      );
+                    },
+                  )),
+                  decoration: BoxDecoration(
+                    color: Colors.blue,
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                ),
+              ]),
             ]),
-          ]),
-    ),);
+      ),
+    );
   }
 }
